b652afc5d6d8152de8040b3bc6ad85d03d536453
Leandrobo <leandrooliver4826@gmail.com>
2025-04-11 17:00:39 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,15 @@ from flask import Flask, render_template
 from auth import auth_blueprint
 from ml import ml_blueprint
 
-app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta'
+app = Flask(__name__, template_folder="../templates")
 
-# Registro dos Blueprints
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(ml_blueprint)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'Plataforma Gustinni online!'
+    return "<h1>Plataforma Gustinni Online</h1><a href='/login'>Ir para Login</a>"
 
-if __name__ == '__main__':
-    app.run(debug=True)
-  
+if __name__ == "__main__":
+    app.run()
+    
