56d071659b82d717828ec4f88289984ba908a1ef
Leandrobo <leandrooliver4826@gmail.com>
2025-04-11 15:29:25 -0300

    Create ml.py

---
+++ backend/ml.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+ml_blueprint = Blueprint('ml', __name__)
+
+@ml_blueprint.route('/ml')
+def ml_view():
+    return 'Funcionalidade de IA em desenvolvimento!'
+
+__all__ = ['ml_blueprint']
